339ab09ba412d17d8158a6544c71454b1de3abb8
Victor <victor.hugomarques@hotmail.com>
2020-12-07 15:24:40 -0300

    orders/create update

--- app.js
+++ app.js
@@ -141,59 +141,45 @@ app.post("/checkouts/create", async (req, res, next) => {
 });
 //
 app.post("/orders/create", async (req, res, next) => {
-  console.log(req.body);
-  /*
   try {
-    let order = [];
-    let created_lead = [];
-    let last_order = await axios({
-        method: "GET", 
-        url: "https://" + process.env.API_KEY + ":" + process.env.API_PASSWORD + "@" + process.env.API_SHOP + "/admin/api/2020-10/orders/" + req.body.id + ".json",
-    });
+    console.log(req.body);
     //
-    order.push(last_order);
-    //
-    order.forEach((obj) => {
-      created_lead.push({
-        "event_type": "CONVERSION",
-        "event_family":"CDP",
-        "payload": {
-          "conversion_identifier": "Pagamento-Pendente",
-          "name": obj.customer.name,
-          "email": obj.customer.email,
-          "job_title": "",
-          "state": obj.customer.billing_province,
-          "city": obj.customer.billing_city,
-          "country": obj.customer.billing_country,
-          "personal_phone": "",
-          "mobile_phone": obj.customer.phone,
-          "cf_price": obj.total,
-          "cf_order_payment_method": obj.payment_details.method,
-          "cf_status": "pendente",
-          "cf_order_id": JSON.stringify(obj.id),
-          "twitter": "",
-          "facebook": "",
-          "linkedin": "",
-          "website": "",
-          "cf_custom_field_api_identifier": JSON.stringify(obj.customer.identification),
-          "company_name": "",
-          "company_site": "",
-          "company_address": "",
-          "client_tracking_id": JSON.stringify(obj.customer.id),
-          "traffic_source": "Nuvemshop",
-          "traffic_medium": "",
-          "traffic_campaign": "",
-          "traffic_value": "",
-          "tags": ["nuvemshop", "rocklola"],
-          "available_for_mailing": true,
-        }
-      });
-      if(obj.payment_details.method == "credit_card") created_lead[0].payload.cf_order_payment_method = "Cartão de Crédito"
-      else if(obj.payment_details.method == "offline") created_lead[0].payload.cf_order_payment_method = "Boleto"
-    });
-    //
-    console.log(created_lead);
-    //
+    let order_create;
+    order_create = {
+      "event_type": "CONVERSION",
+      "event_family":"CDP",
+      "payload": {
+        "conversion_identifier": "Pagamento-Pendente",
+        "name": req.body.customer.first_name + " " + req.body.customer.last_name,
+        "email": req.body.email,
+        "job_title": "",
+        "state": req.body.customer.default_address.province,
+        "city": req.body.customer.default_address.city,
+        "country": req.body.customer.default_address.country,
+        "personal_phone": req.body.customer.default_address.phone,
+        "mobile_phone": req.body.customer.phone,
+        "cf_orders_count": JSON.stringify(req.body.customer.orders_count),
+        "cf_total_spent": req.body.customer.total_spent,
+        "cf_checkout_url": "",
+        "cf_order_status_url": req.body.order_status_url, 
+        "twitter": "",
+        "facebook": "",
+        "linkedin": "",
+        "website": "",
+        "company_name": "",
+        "company_site": "",
+        "company_address": "",
+        "client_tracking_id": "",
+        "traffic_source": req.body.referring_site,
+        "traffic_medium": "",
+        "traffic_campaign": "",
+        "traffic_value": "",
+        "tags": ["shopify", "anna-prata"],
+        "available_for_mailing": true,
+      }
+    }
+    console.log(order_create);
+    // 
     let authentication = await axios({ method: 'POST', url: process.env.RD_AUTH_URL,
       header: {
         "Content-Type": "application/json"
@@ -204,22 +190,19 @@ app.post("/orders/create", async (req, res, next) => {
         "refresh_token": process.env.RD_REFLESH_TOKEN
       }
     });
-    created_lead.forEach(async (item) => {
-      console.log(item)
-      let req = await axios({ method: 'POST', url: process.env.RD_LEAD_URL,
-        headers: {
-          "Authorization": "Bearer " + authentication.data.access_token,
-          "Content-Type": "application/json"
-        },
-        data: item
-      });
+    //
+    let create_order = await axios({ method: 'POST', url: process.env.RD_LEAD_URL,
+      headers: {
+        "Authorization": "Bearer " + authentication.data.access_token,
+        "Content-Type": "application/json"
+      },
+      data: order_create
     });
-    return res.status(200).send('Lead criada com sucesso.');
-  } catch (error) {
-      return res.status(500).send('Erro ao criar a lead.');
+    console.log(create_order.data);
+    return res.status(200).send('Ok');
+  } catch(error) {
+    return res.status(500).send('Erro ao criar a lead.');
   }
-  */
- return res.status(200).send('Ok');
 })
 //
 app.post("/orders/paid", async (req, res, next) => {
