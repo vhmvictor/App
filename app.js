5735818a1efa64dfba69b672088fa3a7b584e31f
Victor <victor.hugomarques@hotmail.com>
2020-12-07 15:44:01 -0300

    orders/paid update

--- app.js
+++ app.js
@@ -206,65 +206,56 @@ app.post("/orders/create", async (req, res, next) => {
 })
 //
 app.post("/orders/paid", async (req, res, next) => {
-  console.log(req.body);
-  /*
   try {
-    nuvemshop.config({
-      store_id: process.env.STORE_ID,
-      access_token: process.env.ACCESS_TOKEN,
-      user_agent: process.env.USER_AGENT
-    });
-    let order = [];
-    order.push(await nuvemshop.getOrder(req.body.id));
-    let created_lead = [];
-    let lead_paid_order = [];
-    order.forEach((obj) => {
-      created_lead.push({
-        "event_type": "CONVERSION",
-        "event_family":"CDP",
-        "payload": {
-          "conversion_identifier": "Pagamento-Finalizado",
-          "name": obj.customer.name,
-          "email": obj.customer.email,
-          "job_title": "",
-          "state": obj.customer.billing_province,
-          "city": obj.customer.billing_city,
-          "country": obj.customer.billing_country,
-          "personal_phone": "",
-          "mobile_phone": obj.customer.phone,
-          "cf_price": obj.total,
-          "cf_order_payment_method": "",
-          "cf_status": "aprovado",
-          "cf_order_id": JSON.stringify(obj.id),
-          "twitter": "",
-          "facebook": "",
-          "linkedin": "",
-          "website": "",
-          "cf_custom_field_api_identifier": JSON.stringify(obj.customer.identification),
-          "company_name": "",
-          "company_site": "",
-          "company_address": "",
-          "client_tracking_id": JSON.stringify(obj.customer.id),
-          "traffic_source": "Nuvemshop",
-          "traffic_medium": "",
-          "traffic_campaign": "",
-          "traffic_value": "",
-          "tags": ["nuvemshop", "processo-concluído", "rocklola"],
-          "available_for_mailing": true,
-        }
-      });
-      if(obj.payment_details.method == "credit_card") created_lead[0].payload.cf_order_payment_method = "Cartão de Crédito"
-      else if(obj.payment_details.method == "offline") created_lead[0].payload.cf_order_payment_method = "Boleto"
-      lead_paid_order.push({
-        "event_type": "SALE",
-        "event_family":"CDP",
-        "payload": {
-          "email": obj.customer.email,
-          "funnel_name": "default",
-          "value": parseFloat(obj.total)
-        }
-      });
-    });
+    console.log(req.body);
+    //
+    let order_paid;
+    let sale;
+    order_paid = {
+      "event_type": "CONVERSION",
+      "event_family":"CDP",
+      "payload": {
+        "conversion_identifier": "Pagamento-Finalizado",
+        "name": req.body.customer.first_name + " " + req.body.customer.last_name,
+        "email": req.body.email,
+        "job_title": "",
+        "state": req.body.customer.default_address.province,
+        "city": req.body.customer.default_address.city,
+        "country": req.body.customer.default_address.country,
+        "personal_phone": req.body.customer.default_address.phone,
+        "mobile_phone": req.body.customer.phone,
+        "cf_orders_count": JSON.stringify(req.body.customer.orders_count),
+        "cf_total_spent": req.body.customer.total_spent,
+        "cf_checkout_url": "",
+        "cf_order_status_url": req.body.order_status_url, 
+        "twitter": "",
+        "facebook": "",
+        "linkedin": "",
+        "website": "",
+        "company_name": "",
+        "company_site": "",
+        "company_address": "",
+        "client_tracking_id": "",
+        "traffic_source": req.body.referring_site,
+        "traffic_medium": "",
+        "traffic_campaign": "",
+        "traffic_value": "",
+        "tags": ["processo-concluído"],
+        "available_for_mailing": true,
+      }
+    }
+    console.log(order_paid);
+    //
+    sale = {
+      "event_type": "SALE",
+      "event_family":"CDP",
+      "payload": {
+        "email": req.body.customer.email,
+        "funnel_name": "default",
+        "value": parseFloat(req.body.total_price)
+      }
+    };
+    // 
     let authentication = await axios({ method: 'POST', url: process.env.RD_AUTH_URL,
       header: {
         "Content-Type": "application/json"
@@ -276,34 +267,27 @@ app.post("/orders/paid", async (req, res, next) => {
       }
     });
     //
-    console.log(created_lead);
+    let create_order = await axios({ method: 'POST', url: process.env.RD_LEAD_URL,
+      headers: {
+        "Authorization": "Bearer " + authentication.data.access_token,
+        "Content-Type": "application/json"
+      },
+      data: order_paid
+    });
+    console.log(create_order.data);
     //
-    created_lead.forEach(async (item) => {
-      console.log(item)
-      let request = await axios({ method: 'POST', url: process.env.RD_LEAD_URL,
-        headers: {
-          "Authorization": "Bearer " + authentication.data.access_token,
-          "Content-Type": "application/json"
-        },
-        data: item
-      });
+    let create_sale = await axios({ method: 'POST', url: process.env.RD_LEAD_URL,
+      headers: {
+        "Authorization": "Bearer " + authentication.data.access_token,
+        "Content-Type": "application/json"
+      },
+      data: sale
     });
-    lead_paid_order.forEach(async (item) => {
-      console.log(item)
-      let request = await axios({ method: 'POST', url: process.env.RD_LEAD_URL,
-        headers: {
-          "Authorization": "Bearer " + authentication.data.access_token,
-          "Content-Type": "application/json"
-        },
-        data: item
-      });
-    });
-    return res.status(200).send('Lead criada com sucesso.');
-  } catch (error) {
-      return res.status(500).send('Erro ao criar a lead.');
+    console.log(create_sale.data);
+    return res.status(200).send('Ok');
+  } catch(error) {
+    return res.status(500).send('Erro ao criar a lead.');
   }
-  */
-  return res.status(200).send('Ok');
 })
 //
 app.listen(process.env.PORT || 3000, () => {
